47aff1aaf59882495a8e2faf6b41751f39aefb64
simonkrol <simonkrol11@gmail.com>
2017-02-24 01:24:26 -0500

    rename script and gui files

---
+++ bagged_lunch_gui.py
@@ -0,0 +1,8 @@
+import tkinter
+top=tkinter.Tk()    #Creates Tkinter
+"""Create a calendar within that allows for selection of multiple dates
+Find a way to have 3 clickable areas per calendar day to allow for choices between
+breakfast, lunch and dinner. Alternate through colours representing seperate meals 
+and show meals on sidebar
+"""
+top.mainloop()  #Opens the gui
